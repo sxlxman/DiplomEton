d5c7e3c725d38ded37e1250a55a182ac1b94e797
sxlxman <asolodaskina@gmail.com>
2025-05-28 18:31:11 +0300

    первый коммит

---
+++ Gofroychetqq/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gofroychetqq
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private User _currentUser;
+        public MainWindow(User user)
+        {
+            InitializeComponent();
+            _currentUser = user;
+            if (_currentUser != null && _currentUser.Role != null)
+            {
+                UserInfoText.Text = $"Пользователь: {_currentUser.Surname} {_currentUser.Name} {_currentUser.Patronymic} | Роль: {_currentUser.Role.Name}";
+            }
+            else if (_currentUser != null)
+            {
+                UserInfoText.Text = $"Пользователь: {_currentUser.Surname} {_currentUser.Name} {_currentUser.Patronymic}";
+            }
+            // Загрузка фото
+            if (_currentUser != null && !string.IsNullOrEmpty(_currentUser.Photo))
+            {
+                try
+                {
+                    var image = new BitmapImage(new Uri(_currentUser.Photo, UriKind.RelativeOrAbsolute));
+                    UserPhoto.Source = image;
+                }
+                catch
+                {
+                    UserPhoto.Source = null;
+                }
+            }
+            else
+            {
+                UserPhoto.Source = null;
+            }
+        }
+        public MainWindow() : this(null) { }
+    }
+}
